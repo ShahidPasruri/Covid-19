f2db49e36dbfa03a21fb70023cd735f3064e1c0c
ShahidPasruri <spasruri2@gmail.com>
2020-04-11 08:59:10 +0500

    Updates

---
+++ precache-manifest.0ed27b402fd4e6e494ea056b65d46534.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784366897a4d67ef75b6ad1e02b48cea",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "2c9d92a75be99eb7342a",
+    "url": "/Covid-19/static/css/main.8d082f91.chunk.css"
+  },
+  {
+    "revision": "0ff9477d5688b3f3ee46",
+    "url": "/Covid-19/static/js/2.508ebe4a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.508ebe4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9d92a75be99eb7342a",
+    "url": "/Covid-19/static/js/main.1f3dc9c9.chunk.js"
+  },
+  {
+    "revision": "8b236949ff99192dca5b",
+    "url": "/Covid-19/static/js/runtime-main.be194e69.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/covid-19.d7265326.png"
+  }
+]);
